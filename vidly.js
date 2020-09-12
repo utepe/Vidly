b7299880eb2421967c5ba3298317f9006d3512ee
Uygur Tepe <uygur.t10@gmail.com>
2020-09-12 19:32:09 -0400

    finished code

--- vidly.js
+++ vidly.js
@@ -15,14 +15,20 @@ const genres = [
 ];
 
 app.get('/api/genres/', (req, res) => {
-    res.send(genre)
+    res.send(genres)
+});
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    res.send(genre);
 });
 
 app.post('/api/genres/', (req, res) => {
    const { error } = validateGenre(req.body);
    if(error) return res.status(400).send(error.details[0].message);
 
-    const genres = {
+    const genre = {
         id: genres.length+1,
         name: req.body.name
     };
@@ -34,7 +40,7 @@ app.post('/api/genres/', (req, res) => {
 app.put('/api/genres/:id', (req, res) => {
     // Lookup the course
     // If not existing, return 404
-    const genre = genres.find(g => g.id === parseInt(req.param.id));
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
     if(!genre) return res.status(404).send('The genre with the given ID was not found');
 
     // Validate
@@ -51,7 +57,7 @@ app.put('/api/genres/:id', (req, res) => {
 app.delete('/api/genres/:id', (req, res) => {
     // Lookup the course
     // If not existing, return 404
-    const genre = genres.find(g => g.id === parseInt(req.param.id));
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
     if(!genre) return res.status(404).send('The genre with the given ID was not found');
 
     // Validate
@@ -62,9 +68,18 @@ app.delete('/api/genres/:id', (req, res) => {
     // Delete
     const index = genres.indexOf(genre);
     genres.splice(index, 1);
-
+    // for(let i = 0; i < genres.length; i++){
+    //     genres[i].id--;
+    // }
     res.send(genre);
 });
+
+function validateGenre(genre){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(genre);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`)});
