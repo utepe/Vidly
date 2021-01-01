7a148a2c3c9bd8a64850555ce6afd6c6bbd9941f
utepe <uygur.t10@gmail.com>
2021-01-01 08:28:58 -0500

    updated elements

--- routes/genres.js
+++ routes/genres.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const genres = [
-    { id: 1, name: 'action'},
-    { id: 2, name: 'adventure'}
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Horror' },
+    { id: 3, name: 'Romance' }
 ];
 
 router.get('/', (req, res) => {
@@ -17,11 +18,11 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-   const { error } = validateGenre(req.body);
-   if(error) return res.status(400).send(error.details[0].message);
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
     const genre = {
-        id: genres.length+1,
+        id: genres.length + 1,
         name: req.body.name
     };
 
@@ -33,12 +34,12 @@ router.put('/:id', (req, res) => {
     // Lookup the course
     // If not existing, return 404
     const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The genre with the given ID was not found');
+    if (!genre) return res.status(404).send('The genre with the given ID was not found');
 
     // Validate
     // If invalid, return 400, Bad Request    
     const { error } = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
     // Update Course
     // Return updated course    
@@ -50,7 +51,7 @@ router.delete('/:id', (req, res) => {
     // Lookup the course
     // If not existing, return 404
     const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The genre with the given ID was not found');
+    if (!genre) return res.status(404).send('The genre with the given ID was not found');
 
     // Delete
     const index = genres.indexOf(genre);
@@ -61,7 +62,7 @@ router.delete('/:id', (req, res) => {
     res.send(genre);
 });
 
-function validateGenre(genre){
+function validateGenre(genre) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
