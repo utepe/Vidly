630fa2ac4026c624cb56428fdb75d1181038b6ab
utepe <uygur.t10@gmail.com>
2021-01-01 15:53:35 -0500

    added movie route and model

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('Joi');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50
     }
-}));
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
     const schema = Joi.object({
@@ -17,5 +19,6 @@ function validateGenre(genre) {
     return schema.validate(genre);
 }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
