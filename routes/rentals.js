620a8f7d0b6c66f77a227974dd1a20bdd903769f
utepe <uygur.t10@gmail.com>
2021-01-01 20:06:46 -0500

    added Fawn task

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,12 @@
-const { Rental, validate } = require('../models/rental')
-const { Movie } = require('../models/movie')
-const { Customer } = require('../models/customer')
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
+const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
@@ -41,12 +45,18 @@ router.post('/', async (req, res) => {
             dailyRentalRate: movie.dailyRentalRate
         }
     });
-    rental = await rental.save();
 
-    movie.numberInStock--;
-    movie.save();
-    
-    res.send(rental);
+    try {
+        new Fawn.Task()
+            .save('rentals', rental)
+            .update('movies', { _id: movie._id }, {
+                $inc: { numberInStock: -1 }
+            })
+            .run();
+        res.send(rental);
+    } catch (ex) {
+        res.status(500).send('Something failed.');
+    }
 });
 
 module.exports = router;
