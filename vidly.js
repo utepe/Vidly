f65246247a5b52d68c2211a2a943f44e56bb4432
utepe <uygur.t10@gmail.com>
2021-01-04 16:07:59 -0500

    added authorization

--- vidly.js
+++ vidly.js
@@ -10,7 +10,8 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 const express = require('express');
 const app = express();
@@ -25,6 +26,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {console.log(`Listening on port ${port}...`)});
