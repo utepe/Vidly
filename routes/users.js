2eabf8b174ee0dd4998dcf1cc24c8a4f5ce82ee2
utepe <uygur.t10@gmail.com>
2021-01-04 12:57:24 -0500

    added password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
-const { User, validate } = require('../models/user')
+const _ = require('lodash');
+const bcryptjs = require('bcryptjs');
+const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
@@ -9,14 +11,13 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.')
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(user.password, salt);
+    
     await user.save();
 
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
