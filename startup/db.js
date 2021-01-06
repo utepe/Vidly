bc169836687d26c07c974f9a9445ff0f1ff6dfea
utepe <uygur.t10@gmail.com>
2021-01-06 09:13:51 -0500

    added error handling and logging

---
+++ startup/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const winston = require('winston');
+
+module.exports = function () {
+    mongoose.connect('mongodb://localhost:27017/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => winston.info('Connected to MongoDB...'))
+}
--- vidly.js
+++ vidly.js
@@ -1,37 +1,12 @@
-require('express-async-errors');
-const config = require('config');
-const Joi = require('Joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error')
-
+const winston = require('winston');
 const express = require('express');
 const app = express();
 
-if(!config.get('jwtPrivateKey')){
-    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-    process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost:27017/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(error => console.error('Could not connect to MongoDB: ', error));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`Listening on port ${port}...`)});
+app.listen(port, () => { winston.info(`Listening on port ${port}...`) });
