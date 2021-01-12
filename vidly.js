93a074f123353b18307bb23c31bcf26645167cdd
Uygur Tepe <uygur.t10@gmail.com>
2021-01-11 20:34:08 -0500

    added easy localhost clickable link

--- vidly.js
+++ vidly.js
@@ -10,4 +10,4 @@ require('./startup/validation')();
 require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { winston.info(`Listening on port ${port}...`) });
+app.listen(port, () => { winston.info(`Listening on port http://localhost:${port}...`) });
